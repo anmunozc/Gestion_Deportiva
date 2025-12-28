82c8e98eae63cac0c735f6f8eadab3970301a715
AMC <anmunozc@gmail.com>
2025-12-27 22:59:05 -0300

    feat: conexion a la bd

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "gestion_deportiva";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function getConnection() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
+        } catch(PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
