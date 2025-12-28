b488159ff5dc2954bc994a47625ef85ba18caccf
AMC <anmunozc@gmail.com>
2025-12-27 23:20:00 -0300

    feat: archivo AuthController.php

---
+++ controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+require_once "models/Usuario.php";
+
+class AuthController {
+
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function login($email, $password) {
+
+        $usuario = new Usuario($this->db);
+        $resultado = $usuario->login($email, $password);
+
+        if ($resultado) {
+            $_SESSION['usuario'] = $resultado;
+
+            if ($resultado['rol'] === 'entrenador') {
+                header("Location: index.php?action=panel_entrenador");
+                exit;
+            }
+
+            header("Location: index.php");
+            exit;
+        }
+
+        header("Location: index.php?error=1");
+        exit;
+    }
+
+    public static function verificarSesion() {
+        if (!isset($_SESSION['usuario'])) {
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    public static function verificarRol($rol) {
+        if ($_SESSION['usuario']['rol'] !== $rol) {
+            echo "Acceso no autorizado";
+            exit;
+        }
+    }
+}
